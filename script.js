f85c1f069ff4022645d48f414ec90046ac148c67
Anamika <115761445+Anamikabisht@users.noreply.github.com>
2024-11-09 13:49:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const token = "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"; // Replace with your actual Hugging Face API token
+const inputText = document.getElementById("input");
+const image = document.getElementById("image");
+const button = document.getElementById("btn");
+
+async function query() {
+	image.src="loading.gif"
+    try {
+        const response = await fetch(
+            "https://api-inference.huggingface.co/models/kothariyashhh/GenAi-Texttoimage",
+            {
+                headers: { Authorization: `Bearer ${token}` },
+                method: "POST",
+                body: JSON.stringify({ "inputs": inputText.value }),
+            }
+        );
+
+        if (!response.ok) {
+            throw new Error("Failed to fetch image from API");
+        }
+
+        const result = await response.blob(); // Convert the response to a blob
+        return result;
+    } catch (error) {
+        console.error("Error:", error);
+        alert("An error occurred while generating the image. Please try again.");
+    }
+}
+
+button.addEventListener("click", async function() {
+    const response = await query();
+    if (response) {
+        const objURL = URL.createObjectURL(response);
+        image.src = objURL;
+    }
+});
